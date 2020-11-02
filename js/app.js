ece2e9d7280f3e1dd85d0c1610cd3dfe700ccf1c
James Gerstenberger <gerstej9@gmail.com>
2020-11-02 15:11:59 -0800

    add link to sales.html on index.html and create function to calculate total sales and amend function for adding total sales to li

--- js/app.js
+++ js/app.js
@@ -1,33 +1,43 @@
 hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', 
-'1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm' ];
+'1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', 'Total' ];
 var seattle = {
     minHourCustomer: 23,
     maxHourCustomer: 65,
     averageCookies: 6.3,
+    saleArray:[],
+    total: 0,
 }
 
 var tokyo = {
     minHourCustomer: 3,
     maxHourCustomer: 24,
     averageCookies: 1.2,
+    saleArray: [],
+    total: 0,
 }
 
 var dubai = {
     minHourCustomer: 11,
     maxHourCustomer: 38,
     averageCookies: 3.7,
+    saleArray: [],
+    total: 0,
 }
 
 var paris = {
     minHourCustomer: 20,
     maxHourCustomer: 38,
     averageCookies: 2.3,
+    saleArray: [],
+    total: 0,
 }
 
 var lima = {
     minHourCustomer: 2,
     maxHourCustomer: 16,
     averageCookies: 4.6,
+    saleArray: [],
+    total: 0,
 }
 
 
@@ -42,25 +52,37 @@ var cityCookiesMethods = {
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive 
     },
-    saleArray: [],
+    // saleArray: [],
 
     hourSales: function(city, averageCookies){
-        for(var i = 0; i<hours.length; i++){
+        for(var i = 0; i<hours.length-1; i++){
             var customer = cityCookiesMethods.randomInt(city.minHourCustomer, city.maxHourCustomer);
             var totalCookies = Math.floor(customer * city.averageCookies);
-            cityCookiesMethods.saleArray.push(totalCookies);
+            city.saleArray.push(totalCookies);
         }
+    },
+
+    totalSales: function(city){
+        for(var i = 0; i<hours.length-1; i++){
+            city.total = city.total+ city.saleArray[i];
+        }
+        return city.total
     },
 
 
     liAdd: function (city, id){
-        for(var i = 0; i<hours.length; i++){
+        for(var i = 0; i<hours.length-1; i++){
             var salesParent = document.getElementById(id);
             //console.log(salesParent);
             var liElement = document.createElement('li')
-            liElement.textContent = hours[i] + ': ' + cityCookiesMethods.saleArray[i] + ' cookies';
+            liElement.textContent = hours[i] + ': ' + city.saleArray[i] + ' cookies';
             salesParent.appendChild(liElement);
         }
+        var salesParent = document.getElementById(id);
+        //console.log(salesParent);
+        var liElement = document.createElement('li')
+        liElement.textContent = hours[hours.length-1] + ': ' + city.total + ' cookies';
+        salesParent.appendChild(liElement);
     },
 
 }
@@ -69,6 +91,7 @@ var cityCookiesMethods = {
 var cityCookies = function(city, cityCookiesMethods, id){
 cityCookiesMethods.randomInt(city.minHourCustomer, city.maxHourCustomer);
 cityCookiesMethods.hourSales(city, city.averageCookies);
+cityCookiesMethods.totalSales(city, city.total);
 cityCookiesMethods.liAdd(city,id);
 };
 cityCookies(seattle, cityCookiesMethods, 'seattle');
